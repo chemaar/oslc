d6621c2d905e7ccc28a4d82d553081639513f537
Jose María Alvarez <chema.ar@gmail.com>
2013-09-27 11:16:19 +0200

    Rename file

---
+++ apps/oslc-rm/src/main/java/es/uc3m/inf/kr/oslcrm/Constants.java
@@ -0,0 +1,35 @@
+package es.uc3m.inf.kr.oslcrm;
+
+import org.eclipse.lyo.oslc4j.core.model.OslcConstants;
+
+public interface Constants
+{
+    public static String REQUIREMENTS_MANAGEMENT_NAMESPACE           = "http://open-services.net/ns/rm#";
+    public static String REQUIREMENTS_MANAGEMENT_PREFIX              = "oslc_rm";
+    public static String SOFTWARE_CONFIGURATION_MANAGEMENT_NAMESPACE = "http://open-services.net/ns/scm#";
+    public static String SOFTWARE_CONFIGURATION_MANAGEMENT_PREFIX    = "oslc_scm";
+    public static String BUGZILLA_DOMAIN							 = "http://www.bugzilla.org/rdf#"; 
+    public static String BUGZILLA_NAMESPACE							 = "http://www.bugzilla.org/rdf#";
+    public static String BUGZILLA_NAMESPACE_PREFIX					 = "bugz";
+
+/*
+    public static String CHANGE_REQUEST             = "ChangeRequest";
+    public static String TYPE_CHANGE_REQUEST        = CHANGE_MANAGEMENT_NAMESPACE + "ChangeRequest";
+    public static String TYPE_CHANGE_SET            = SOFTWARE_CONFIGURATION_MANAGEMENT_NAMESPACE + "ChangeSet";
+    public static String TYPE_DISCUSSION            = OslcConstants.OSLC_CORE_NAMESPACE + "Discussion";
+    public static String TYPE_PERSON                = FOAF_NAMESPACE + "Person";
+    public static String TYPE_REQUIREMENT           = REQUIREMENTS_MANAGEMENT_NAMESPACE + "Requirement";
+    public static String TYPE_TEST_CASE             = QUALITY_MANAGEMENT_NAMESPACE + "TestCase";
+    public static String TYPE_TEST_EXECUTION_RECORD = QUALITY_MANAGEMENT_NAMESPACE + "TestExecutionRecord";
+    public static String TYPE_TEST_PLAN             = QUALITY_MANAGEMENT_NAMESPACE + "TestPlan";
+    public static String TYPE_TEST_RESULT           = QUALITY_MANAGEMENT_NAMESPACE + "TestResult";
+    public static String TYPE_TEST_SCRIPT           = QUALITY_MANAGEMENT_NAMESPACE + "TestScript";
+
+    public static String PATH_CHANGE_REQUEST = "changeRequest";
+
+    public static String USAGE_LIST = CHANGE_MANAGEMENT_NAMESPACE + "list";
+    
+    public static final String HDR_OSLC_VERSION = "OSLC-Core-Version";
+    
+    public static final String NEXT_PAGE = "org.eclipse.lyo.oslc4j.bugzilla.NextPage";*/
+}
