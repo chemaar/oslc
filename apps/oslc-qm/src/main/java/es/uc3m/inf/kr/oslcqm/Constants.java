3a2c818658cc366af1e614e0d91827d4ae0ec8c5
Jose María Alvarez <chema.ar@gmail.com>
2013-09-27 10:59:54 +0200

    Adding files

--- apps/oslc-qm/src/main/java/es/uc3m/inf/kr/oslcqm/Constants.java
+++ apps/oslc-qm/src/main/java/es/uc3m/inf/kr/oslcqm/Constants.java
@@ -1,4 +1,4 @@
-package es.uc3m.inf.kr.oslcrm;
+package es.uc3m.inf.kr.oslcqm;
 
 import org.eclipse.lyo.oslc4j.core.model.OslcConstants;
 
---
+++ apps/oslc-qm/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title>OSLC-QM Prototype</title>
+</head>
+<body>
+<h2>OSLC-QM Prototype</h2>
+</body>
+</html>
