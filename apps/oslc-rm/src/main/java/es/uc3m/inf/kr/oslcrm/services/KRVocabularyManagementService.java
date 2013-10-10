30f68bb8cef9aa5323c20a148a388a342c8324d3
Jose María Alvarez <chema.ar@gmail.com>
2013-10-10 19:00:47 +0200

    Modifying classes

--- apps/oslc-rm/src/main/java/es/uc3m/inf/kr/oslcrm/services/KRVocabularyManagementService.java
+++ apps/oslc-rm/src/main/java/es/uc3m/inf/kr/oslcrm/services/KRVocabularyManagementService.java
@@ -39,57 +39,62 @@ public class KRVocabularyManagementService {
 	@Context private HttpServletRequest httpServletRequest;
 	@Context private HttpServletResponse httpServletResponse;
 	@Context private UriInfo uriInfo;
-	
+
 	public KRVocabularyManagementService(){
 		super();
 	}
 
-	 @OslcDialogs(
-			    {
-			        @OslcDialog
-			        (
-			             title = "Requirements Management Selection Dialog",
-			             label = "Requirements Management Selection Dialog",
-			             //uri = "/{productId}/requirementManagement/selector",
-					     uri = "/requirementManagement/selector",
-			             hintWidth = "525px",
-			             hintHeight = "325px",
-			             resourceTypes = {Constants.TYPE_CHANGE_REQUEST},
-			             usages = {OslcConstants.OSLC_USAGE_DEFAULT}
-			        )
+	@OslcDialogs(
+			{
+				@OslcDialog
+				(
+						title = "Requirements Management Selection Dialog",
+						label = "Requirements Management Selection Dialog",
+						//uri = "/{productId}/requirementManagement/selector",
+						uri = "/requirementManagement/selector",
+						hintWidth = "525px",
+						hintHeight = "325px",
+						resourceTypes = {Constants.TYPE_CHANGE_REQUEST},
+						usages = {OslcConstants.OSLC_USAGE_DEFAULT}
+						)
 
-			    })
-			    @OslcQueryCapability
-			    (
-			        title = "Requirements Management Query Capability",
-			        label = "Requirements Management Catalog Query",
-			        resourceShape = OslcConstants.PATH_RESOURCE_SHAPES + "/" + Constants.PATH_CHANGE_REQUEST,
-			        resourceTypes = {Constants.TYPE_CHANGE_REQUEST},
-			        usages = {OslcConstants.OSLC_USAGE_DEFAULT}
-			    )
-	 
-	    @GET
-	    @Produces({OslcMediaType.APPLICATION_RDF_XML, OslcMediaType.APPLICATION_XML, OslcMediaType.APPLICATION_JSON})
-	    public List<VocabularyTO> getChangeRequests() throws IOException, ServletException {
-	    
-		 List<VocabularyTO> requirements = new LinkedList<VocabularyTO>();
-		 VocabularyTO req = new VocabularyTO();
+			})
+	@OslcQueryCapability
+	(
+			title = "Requirements Management Query Capability",
+			label = "Requirements Management Catalog Query",
+			resourceShape = OslcConstants.PATH_RESOURCE_SHAPES + "/" + Constants.PATH_CHANGE_REQUEST,
+			resourceTypes = {Constants.TYPE_CHANGE_REQUEST},
+			usages = {OslcConstants.OSLC_USAGE_DEFAULT}
+			)
+
+	@GET
+	@Produces({OslcMediaType.APPLICATION_RDF_XML, OslcMediaType.APPLICATION_XML, OslcMediaType.APPLICATION_JSON})
+	public List<VocabularyTO> getVocabularyConcepts() throws IOException, ServletException {
+
+		List<VocabularyTO> requirements = new LinkedList<VocabularyTO>();
+
 		// req.setDefinition("My cool definition");
-		 try {
-			req.setAbout(new URI("http://uri.req"));	
-			req.setDefinition("My definition");
-			Person contributor = new Person();
-			contributor.setName("Jose");
-			contributor.setMbox("mailto:josemaria.alvarez@uc3m.es");
-			contributor.setUri(new URI("http://josemalvarez.es/foaf.rdf#me"));
-			req.addContributor(contributor );
+		try {
+			for(int i = 0; i<10;i++){
+				VocabularyTO req = new VocabularyTO();
+				req.setAbout(new URI("http://uri.req/"+i));	
+				req.setDefinition("My definition");
+				Person contributor = new Person();
+				contributor.setName("Jose");
+				contributor.setMbox("mailto:josemaria.alvarez@uc3m.es");
+				contributor.setUri(new URI("http://josemalvarez.es/foaf.rdf#me"));
+				req.addContributor(contributor );
+				requirements.add(req);
+			}
+
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 requirements.add(req);
-		 return requirements;
-	 }
-	    
-	  
+
+		return requirements;
+	}
+
+
 }
