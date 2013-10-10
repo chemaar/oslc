a8753237e3e3f3fc31e506230d068a4aadb584e8
Jose María Alvarez <chema.ar@gmail.com>
2013-10-10 18:43:34 +0200

    Fix names

---
+++ apps/oslc-rm/src/main/java/es/uc3m/inf/kr/oslcrm/SKOS.java
@@ -0,0 +1,36 @@
+package es.uc3m.inf.kr.oslcrm;
+
+public class SKOS {
+	
+    /** SKOS Namespace and Properties **/
+    
+    public static final String CORE_NAMESPACE = "http://www.w3.org/2004/02/skos/core#";
+    public static final String PREF_LABEL = CORE_NAMESPACE+"prefLabel";
+    public static final String ALT_LABEL = CORE_NAMESPACE+"altLabel";
+    public static final String HIDDEN_LABEL = CORE_NAMESPACE+"hiddenLabel";
+    
+    public static final String CHANGE_NOTE = CORE_NAMESPACE+"changeNote";
+    public static final String EDITORIAL_NOTE = CORE_NAMESPACE+"editorialNote";
+    public static final String HISTORY_NOTE = CORE_NAMESPACE+"historyNote";
+    public static final String SCOPE_NOTE = CORE_NAMESPACE+"scopeNote";
+    
+    public static final String NOTATION = CORE_NAMESPACE+"notation";
+    public static final String NARROWER = CORE_NAMESPACE+"narrower";
+    public static final String BROADER = CORE_NAMESPACE+"broader";
+    public static final String BROADER_TRANSITIVE = CORE_NAMESPACE+"broaderTransitive";
+    public static final String NARROWER_TRANSITIVE = CORE_NAMESPACE+"narrowerTransitive";
+    
+    
+    public static final String MATCH = CORE_NAMESPACE+"match";
+    public static final String CLOSE_MATCH = CORE_NAMESPACE+"closeMatch";
+    public static final String EXACT_MATCH = CORE_NAMESPACE+"exactMatch";
+    
+    public static final String IN_SCHEME = CORE_NAMESPACE+"inScheme";
+    
+    public static final String CONCEPT = CORE_NAMESPACE+"Concept";
+    
+    public static final String CONCEPT_SCHEME = CORE_NAMESPACE+"ConceptScheme";
+    
+    
+    
+}
