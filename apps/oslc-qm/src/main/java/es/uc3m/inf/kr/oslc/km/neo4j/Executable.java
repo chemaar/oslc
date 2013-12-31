d5d681814a8682bc667899c1da488883338d2928
Jose María Alvarez <chema.ar@gmail.com>
2013-12-31 12:34:39 +0100

    Add classes

---
+++ apps/oslc-qm/src/main/java/es/uc3m/inf/kr/oslc/km/neo4j/Executable.java
@@ -0,0 +1,50 @@
+package es.uc3m.inf.kr.oslc.km.neo4j;
+
+
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.HighlyAvailableGraphDatabaseFactory;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.turtle.TurtleParser;
+
+public class Executable {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		if(args.length!=1){
+			throw new Exception("Provide a file path as a parameter!");
+		}
+		
+		File file = new File(args[0]);
+		
+		if(!file.canRead())
+			throw new Exception("Can't read the file.");
+				
+		HashMap<String, String> settings = new HashMap<String, String>();
+		settings.put("org.neo4j.server.database.mode", "HA");
+
+		GraphDatabaseService db = new HighlyAvailableGraphDatabaseFactory()
+				.newHighlyAvailableDatabaseBuilder("db.local")
+				.loadPropertiesFromFile("neo4j.properties").setConfig(settings)
+				.newGraphDatabase();
+		
+		BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+		
+		RDFParser rdfParser = new TurtleParser();
+		
+		Neo4jHandler handler = new Neo4jHandler(db);
+		rdfParser.setRDFHandler(handler);
+		
+		rdfParser.parse(bis, file.toURI().toString());
+		
+		System.out.println(handler.getCountedStatements());
+	}
+
+}
